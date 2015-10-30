aa9a2c239342daf313fead024fda677ab9cd59f9
LatvianModder <latvianmodder@gmail.com>
2015-10-30 14:12:20 +0200

    Added MapEntry

---
+++ src/latmod/lib/MapEntry.java
@@ -0,0 +1,21 @@
+package latmod.lib;
+
+import java.util.Map;
+
+public class MapEntry<K, V> implements Map.Entry<K, V>
+{
+	private K key;
+	private V val;
+	
+	public MapEntry(K k, V v)
+	{ key = k; val = v; }
+	
+	public K getKey()
+	{ return key; }
+	
+	public V getValue()
+	{ return val; }
+	
+	public V setValue(V value)
+	{ val = value; return val; }
+}
