f3cb3adaae2c9e91c7f2d2e2ee6e24c35a00c59d
LatvianModder <latvianmodder@gmail.com>
2015-12-23 23:08:50 +0200

    Added Base64 mirror from Java 1.8, LMURLConnection.LinkBuilder

--- src/latmod/lib/LMURLConnection.java
+++ src/latmod/lib/LMURLConnection.java
@@ -33,6 +33,35 @@ public class LMURLConnection
 		{ return LMStringUtils.readString(stream); }
 	}
 	
+	public static class LinkBuilder
+	{
+		private final StringBuilder string;
+		private boolean first = true;
+		
+		public LinkBuilder(String init)
+		{
+			string = new StringBuilder();
+			if(init != null && !init.isEmpty())
+			{
+				string.append(init);
+				string.append('?');
+			}
+		}
+		
+		public LinkBuilder append(String key, Object val)
+		{
+			if(first) first = false;
+			else string.append('&');
+			string.append(key);
+			string.append('=');
+			string.append(val);
+			return this;
+		}
+		
+		public String toString()
+		{ return string.toString(); }
+	}
+	
 	public final Type type;
 	public final String url;
 	public final ByteIOStream data;
